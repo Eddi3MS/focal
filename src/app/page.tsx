a9425e309b5c76f3195cf9bce37735c5b3493b4d
Eddi3MS <edsonmarc2014@gmail.com>
2024-09-20 21:49:53 -0300

    fix: stale date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 import Todos from '@/components/Todos'
 
 export default function Home() {
-  return (
-    <>
-      <Todos />
-    </>
-  )
+  return <Todos />
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
+'use client'
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './header.module.scss'
 import { getToday } from '@/utils/getToday'
 
-const date = getToday()
-
 export default function Header() {
+  const date = useState(() => getToday())[0]
+
   return (
     <header className={styles.header}>
       <Image src="/logo.png" alt="" width={150} height={36} />
